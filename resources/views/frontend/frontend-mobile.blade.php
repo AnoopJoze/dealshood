3168c05af89b630241abe1adfb0d44d911b6f31a
Anoop Jose <alakatanoop@gmail.com>
2026-06-20 15:57:52 +0400

    changes

--- resources/views/frontend/frontend-mobile.blade.php
+++ resources/views/frontend/frontend-mobile.blade.php
@@ -316,3 +316,12 @@ document.getElementById('filterBackdrop')?.addEventListener('click', function ()
     });
 })();
 </script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-1905M4BG0P"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-1905M4BG0P');
+</script>
